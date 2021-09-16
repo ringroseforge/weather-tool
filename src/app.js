8f1265893d39a504eb4b817ebda36bdd838cc52d
ringroseforge <87462550+ringroseforge@users.noreply.github.com>
2021-09-15 19:31:04 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
 let apiKey = "4f53bb6fbafe6f11bafb0801af7db36a";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Yosemite&appid=${apiKey}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Yosemite,US&appid=${apiKey}`;
+
 console.log(apiUrl);
