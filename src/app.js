cb7b77996ce99f11a80d48b1a7fd695a59300308
ringroseforge <87462550+ringroseforge@users.noreply.github.com>
2021-09-15 21:18:42 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
+function displayTemperature(response) {
+  console.log(response.data.main.temp);
+}
+
 let apiKey = "4f53bb6fbafe6f11bafb0801af7db36a";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Yosemite,US&appid=${apiKey}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Yosemite,US&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
