6f2592dbe23095cd59cad56975732823f938d011
ringroseforge <87462550+ringroseforge@users.noreply.github.com>
2021-09-15 21:36:39 -0700

    updated js

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 function displayTemperature(response) {
-  console.log(response.data.main.temp);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  temperatureElement.innerHTML = math.round(response.data.main.temp);
+  cityElement.innerHTML = response.data.name;
 }
 
 let apiKey = "4f53bb6fbafe6f11bafb0801af7db36a";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Yosemite,US&appid=${apiKey}&units=metric`;
 
-console.log(apiUrl);
 axios.get(apiUrl).then(displayTemperature);
