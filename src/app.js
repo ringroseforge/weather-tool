081a1bea1c2632bb4cd6b7ba18a4a9018c797b74
ringroseforge <87462550+ringroseforge@users.noreply.github.com>
2021-09-15 22:57:17 -0700

    built out js and css

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 function displayTemperature(response) {
+  console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
-  temperatureElement.innerHTML = math.round(response.data.main.temp);
+  let descriptionElement = document.querySelector("#description");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
 }
 
 let apiKey = "4f53bb6fbafe6f11bafb0801af7db36a";
