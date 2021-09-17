d1909d29ee57944d6c71b4b443ae39ddbca12559
ringroseforge <87462550+ringroseforge@users.noreply.github.com>
2021-09-16 19:50:48 -0700

    time js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
+function formatDate(timestamp) {
+  let date = new Date(timestap);
+  let hours = timstamp.getHours();
+  let minutes = date.getMinutes();
+  let day = date.getDay();
+  return "${day}";
+}
+
 function displayTemperature(response) {
   console.log(response.data);
   let temperatureElement = document.querySelector("#temperature");
@@ -5,11 +13,13 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "4f53bb6fbafe6f11bafb0801af7db36a";
