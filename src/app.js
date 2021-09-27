0eb2c793d197f22774d35e1278b2317499cb1f8f
ringroseforge <87462550+ringroseforge@users.noreply.github.com>
2021-09-26 22:06:48 -0700

    improved css

--- src/app.js
+++ src/app.js
@@ -24,7 +24,15 @@ function formatDate(timestamp) {
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
   return days[day];
 }
